51b3d3b00b90a72f319ae0957bbe44b9b34dbb23
Pat Hawks <pat@pathawks.com>
2018-11-25 12:13:38 -0600

    idk

--- codegen.cpp
+++ codegen.cpp
@@ -2,8 +2,9 @@
  * CodeGen Redux
  */
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <sstream>
 
 #include <llvm/LinkAllIR.h>
 #include <llvm/IRReader/IRReader.h>
@@ -32,16 +33,48 @@ class Codegen {
 	}
 };
 
-Module& open() {
-	SMDiagnostic error;
-	LLVMContext context;
-	return *(parseIRFile("HelloWorld.bc", error, context).get());
+string op(User *v) {
+	return "constant";
+}
+
+string op(Value *v) {
+	stringstream s;
+	switch(1) {
+	case 0:
+		s << ((APInt*)v)->getLimitedValue();
+	default:
+		s << v->getType()->getPrimitiveSizeInBits()/8;
+	}
+	return s.str();
+}
+
+string compile(Instruction &i) {
+	stringstream s;
+	switch (i.getOpcode()) {
+	case Instruction::Alloca:
+		s << "subq\t$" << op(i.getOperand(0)) << ", %rsp";
+		break;
+	case Instruction::Store:
+		s << "movq\t$" << op(i.getOperand(0)) << ", " << op(i.getOperand(1));
+		break;
+	case Instruction::Load:
+	case Instruction::Call:
+		s << "not yet implemented";
+		break;
+	case Instruction::Ret:
+		s << "retq";
+		break;
+	default:
+		s << "unknown instruction";
+	}
+	s << '\n';
+	return s.str();
 }
 
 int main(int argc, char** argv) {
 	SMDiagnostic error;
 	LLVMContext context;
-	std::unique_ptr<Module> mod = parseIRFile("HelloWorld.bc", error, context, false);
+	std::unique_ptr<Module> mod = parseIRFile("Add.bc", error, context, false);
 	Module *m = mod.get();
 
 	cout << "Platform: " << m->getTargetTriple() << endl;
@@ -50,10 +83,11 @@ int main(int argc, char** argv) {
 	cout << "Source File: " << m->getSourceFileName() << endl;
 
 	for (Function &f: m->functions()) {
+		if (!f.getInstructionCount()) continue;
 		cout << f.getName().str() << ":" << endl;
 		for (BasicBlock &block: f.getBasicBlockList()) {
 			for (Instruction &instruction: block) {
-				outs() << "\t" << instruction << "\n";
+				outs() << "\t" << compile(instruction) << "\t#\t" << instruction << "\n";
 			}
 		}
 	}
