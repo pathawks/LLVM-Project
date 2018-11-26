af0aabb07fdfbb9cebb17ca13ea4fd750afdfc17
Pat Hawks <pat@pathawks.com>
2018-11-26 09:19:14 -0600

    Format Add.c

--- Add.c
+++ Add.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-int add(int x, int y) { return x + y; }
+int add(int x, int y) {
+	return x + y;
+}
 
 int main() {
-  int x = 0;
-  int y = 5;
-  int z = add(x, y);
-  printf("%d\n", z);
-  return 0;
+	int x = 0;
+	int y = 5;
+	int z = add(x, y);
+	printf("%d\n", z);
+	return 0;
 }
