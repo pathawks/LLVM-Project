a92c5e35e62078f97cbb033a0ed861e7717c3c39
Pat Hawks <pat@pathawks.com>
2018-12-02 16:20:57 -0600

    More complicated Loop example

--- examples/Loop.c
+++ examples/Loop.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-	for (int i=0; i<100000; ++i) {
+	puts("First, print 1-10");
+	for (int i=1; i<=10; ++i) {
 		printf("%d\n", i);
+	}
+	puts("\nNow print 1-9");
+	for (int i=1; i<10; ++i) {
+		printf("%u\n", i);
 	}
 	return 0;
 }
