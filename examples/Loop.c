e9be6a5052bf3fb94b943d2bd5e6a8043474a12a
Pat Hawks <pat@pathawks.com>
2018-12-02 18:02:16 -0600

    Add more complicated Loop example

--- examples/Loop.c
+++ examples/Loop.c
@@ -5,9 +5,16 @@ int main(void) {
 	for (int i=1; i<=10; ++i) {
 		printf("%d\n", i);
 	}
+
 	puts("\nNow print 1-9");
 	for (int i=1; i<10; ++i) {
 		printf("%u\n", i);
 	}
+
+	puts("\nNow print even numbers between 2-10");
+	for (int i=2; i<=10; i+=2) {
+		printf("%d\n", i);
+	}
+
 	return 0;
 }
